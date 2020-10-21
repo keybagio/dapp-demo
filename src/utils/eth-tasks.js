08289e7c8d3019b514e389df2f87eac0220328ab
KY <harrykyguo@gmail.com>
2020-10-21 11:05:49 +0800

    fix: 修复web3.eth.sign问题

--- src/utils/eth-tasks.js
+++ src/utils/eth-tasks.js
@@ -145,21 +145,19 @@ const tasks = [
   },
   {
     name: '签名：web3.eth.sign',
-    msg: '0x12345678',
-    exspectResult: '未知',
+    msg: '我是签名的原始内容',
+    exspectResult: '0x639c6e61cdab2a6fa4a3775faf6026e1623a3da380044df07918b2191586c16b28b3a49847b18607cece1f8a88205771fb7333b12e86ac83eb8b648679374db51c',
     test: (task) => {
       return new Promise((resolve, reject) => {
         const address = web3.eth.accounts[0];
-        const msg = task.msg;
+        const msg = web3.sha3(task.msg); // 以太坊中SHA3是指Keccak256
         web3.eth.sign(address, msg, (err, result) => {
-          console.log('web3.eth.sign', err, result);
           if (err) {
             reject(err);
           } else {
-            console.log(`web3.eth.sign - ${msg} =>`, result);
             checkMatch(result, task.exspectResult, resolve, reject)
           }
-        })
+        });
         setTimeout(() => reject('timeout'), timeout);
       })
     },
