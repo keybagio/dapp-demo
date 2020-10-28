7fc8a8ce6df0b059a71b6cf6ac765d23d90d389d
Neego <179763029@qq.com>
2020-10-28 18:32:03 +0800

    Peizhi

--- src/utils/eth-tasks.js
+++ src/utils/eth-tasks.js
@@ -1,12 +1,11 @@
 var ethUtil = require('ethereumjs-util')
 const testAddress = '0x7D2bCd53CFf1d7aE9c232338AA64D262cBe89a30';
 const testKey = '5a1f2cf0a6fafca3997c1b3916fa9ea12a65764972a217ddb6cea2070dfe76e6';
+const eoaAddress = '0x7D2bCd53CFf1d7aE9c232338AA64D262cBe89a30';
+const eoaData = '0x233';
+const ercData = '0x233';
+const contractAddress = 'aa';
 const timeout = 8000;
-
-const contractAddress = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
-const ercData = '0x095ea7b30000000000000000000000007a250d5630b4cf539739df2c5dacb4c659f2488dffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
-const eoaAddress = '0xd58d6a21ce9256a53e3736f62203921cb9667da9';
-const eoaData = '0xcdcd77c000000000000000000000000000000000000000000000000000000000000000450000000000000000000000000000000000000000000000000000000000000001';
 
 // 构造请求体
 const req = (data) => {
